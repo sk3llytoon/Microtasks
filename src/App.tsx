df12833ad6af4c614f281ab721eaab81471827a9
sk3llytoon <hatsz141415@gmail.com>
2022-04-14 23:39:27 +0300

    Microtasks: method map, props, components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Cars} from "./MethodMad/Cars";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const topCars = [
+        {id: 1, manufacturer: 'BMW', model: 'm5cs'},
+        {id: 2, manufacturer: 'Mercedes', model: 'e63s'},
+        {id: 3, manufacturer: 'Audi', model: 'rs6'}
+    ]
+
+    return (
+        <div>
+            <Cars topCars={topCars}/>
+        </div>
+    )
 }
 
 export default App;
