cec5abd08de92350247edbfc7a938cd05270b6dd
sk3llytoon <hatsz141415@gmail.com>
2022-04-15 13:06:09 +0300

    Microtasks: method map, props, components

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import {Cars} from "./MethodMad/Cars";
 function App() {
 
     const topCars = [
-        {id: 1, manufacturer: 'BMW', model: 'm5cs'},
-        {id: 2, manufacturer: 'Mercedes', model: 'e63s'},
-        {id: 3, manufacturer: 'Audi', model: 'rs6'}
+        {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'},
+        {manufacturer: 'Audi', model: 'rs6'}
     ]
 
     return (
